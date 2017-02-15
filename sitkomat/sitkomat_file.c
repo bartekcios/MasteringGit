b385403d10bef44e05ea69a083ba4e07cb1dc859
sitkomat <mateuszsitek.sitkowski@gmail.com>
2017-02-15 21:50:03 +0100

    commit to new branch

--- sitkomat/sitkomat_file.c
+++ sitkomat/sitkomat_file.c
@@ -8,10 +8,19 @@ int random_int(int max)
 	return rand() % max;
 }
 
-int main()
+void init_rand()
 {
 	srand(time(NULL));
-	int sum = random_int(2) + random_int(10);
+}
+
+int main()
+{
+	init_rand();
+	int a = random_int(2);
+	int b = random_int(10);
+	int sum = a + b;
+	int sub = b - a;
+	printf("%d - %d = %d\n", b, a, sub);
 	if (sum > 6)
 	{
 		printf("sum of randoms higher than 6\n");
@@ -20,5 +29,4 @@ int main()
 	{
 		printf("sum of randoms lower or equals 6\n");
 	}
-}
 }
