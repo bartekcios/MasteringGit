263388fa0fe9ad99a81506157c0ef1a76eb7e6ed
Bartosz Ciosek <bartekcios@gmail.com>
2017-02-15 21:53:56 +0100

    Magic modulo function

--- sitkomat/sitkomat_file.c
+++ sitkomat/sitkomat_file.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
+// function returnes modulo value
+int modulo_int(int value1, int value2)
+{
+	int result = 0;
+	if(2 == value2)
+	{
+		result = value1 & 1;
+	}
+	else
+	{
+		result = value1%value2;
+	}
+
+	return result;
+}
+
 // function returnes random value less than max value
 int random_int(int max)
 {
@@ -20,5 +36,6 @@ int main()
 	{
 		printf("sum of randoms lower or equals 6\n");
 	}
-}
+	
+	printf("the fastest modulo counting in the world: %d\n", modulo_int(11, 2));
 }
